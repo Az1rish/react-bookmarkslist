0ab8fdbe7d5568b814f29c8c21a1b24ef168eddd
Shane <shanemcneil19@gmail.com>
2019-09-21 17:53:03 -0700

    add http request and css for AddBookmark component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import './App.css';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkApp from './BookmarkApp/BookmarkApp';
 
-
-const bookmarks = [
-  {
-  title:"Google",
-  url:"http://www.google.com", 
-  rating:"3", 
-  description:"No evil"
-  },
-  {
-    title:"Google",
-    url:"http://www.google.com", 
-    rating:"3", 
-    description:"No evil"
-  }
-];
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -27,13 +11,46 @@ export default class App extends Component {
       showAddForm: false
     };
   }
+
+componentDidMount() {
+  const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+  const options = {
+    method: 'GET',
+    headers: {
+      "Authorization": "Bearer $2a$10$moYWmAFfT1v8CSfi72FHnOAejd0v70CptYUD4M6mUDwvILQP2SGAy",
+      "Content-Type": "application/json"
+    }
+  };
+
+  fetch(url, options)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error('Something went wrong, please try again later.');
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        bookmarks: data,
+        error: null
+      });
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      });
+    });
+
+}
   
   render() {
+    const page = this.state.showAddForm
+      ? <AddBookmark />
+      : <BookmarkApp bookmarks={this.state.bookmarks}/>
     return (
       <div className="App">
-        <AddBookmark />
-        <BookmarkApp bookmarks={bookmarks}/>
-        
+        { page }
       </div>
     );
   }
