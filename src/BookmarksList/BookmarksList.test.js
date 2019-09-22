a143502cfd6dd70e5f22b8d6c5414a09d997c8c9
Shane <shanemcneil19@gmail.com>
2019-09-21 18:03:09 -0700

    correct typos in test file

--- src/BookmarksList/BookmarksList.test.js
+++ src/BookmarksList/BookmarksList.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BookmarkList from './BookmarksList';
+import BookmarksList from './BookmarksList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BookmarkList />, div);
+  ReactDOM.render(<BookmarksList />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
